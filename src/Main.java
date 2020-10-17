99f574bc1901862f5c5fdeeb8c55bd0b8eceead0
yanochi3 <70017926+yanochi3@users.noreply.github.com>
2020-10-17 12:04:57 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,126 @@
 import java.io.*;
+import java.util.*;
 
 
 public class Main {
 	public static void main(String arg[]) {
-		try {    // ƒtƒ@ƒCƒ‹“Ç‚İ‚İ‚É¸”s‚µ‚½‚Ì—áŠOˆ—‚Ì‚½‚ß‚Ìtry-catch\•¶
-            String fileName = "dataset_example.txt"; // ƒtƒ@ƒCƒ‹–¼w’è
-
-            // •¶šƒR[ƒhUTF-8‚ğw’è‚µ‚ÄBufferedReaderƒIƒuƒWƒFƒNƒg‚ğì‚é
+		List<List<List<String>>> list = new ArrayList<List<List<String>>>();
+		int Vmax = 0;
+		int Kmax = 0;
+		int argOfV = 0;
+		int agV[] = new int[arg.length];		//ä½•å¤‰æ•°ã®å¼•æ•°ã‹çŸ¥ã‚ŠãŸã„
+		for(int n = 0; n < arg.length;n++) {
+			agV[n] = 0;
+			String ag[] = arg[n].split(" ",0);
+			for(int a = 0; a < ag.length; a++) {
+				if(ag[a].startsWith("?")) {
+					agV[n]++;
+				}
+			}
+			if(agV[n] > 0) argOfV++;
+		}
+		if(arg.length > 1) {
+			for(int n = 0; n < arg.length-1; n++) {
+				if(agV[n] > agV[n+1]) {
+					agV[n+1] = agV[n];
+					String tmp = arg[n+1];
+					arg[n+1] = arg[n];
+					arg[n] = tmp;
+				}
+			}
+		}
+		
+		try {    // ãƒ•ã‚¡ã‚¤ãƒ«èª­ã¿è¾¼ã¿ã«å¤±æ•—ã—ãŸæ™‚ã®ä¾‹å¤–å‡¦ç†ã®ãŸã‚ã®try-catchæ§‹æ–‡
+            String fileName = "dataset_example.txt"; // ãƒ•ã‚¡ã‚¤ãƒ«åæŒ‡å®š
+            
+            for(int n = 0; n< arg.length; n++) 
+	        {
+            	int countV = 0;
+            	list.add(new ArrayList<List<String>>());
+            // æ–‡å­—ã‚³ãƒ¼ãƒ‰UTF-8ã‚’æŒ‡å®šã—ã¦BufferedReaderã‚ªãƒ–ã‚¸ã‚§ã‚¯ãƒˆã‚’ä½œã‚‹
             BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "UTF-8"));
-            String args = "";
-            for(int i = 0; i< arg.length; i++) 
-            {
-            	args += arg[i]+" ";
-            }
-            // •Ï”line‚É1s‚¸‚Â“Ç‚İ‚Şfor•¶
-            for (String line = in.readLine(); line != null; line = in.readLine()) {
-		            	if((new Unifier()).unify(args,line) == true) 	
-		            	{
-		            	System.out.println(line);
-		            	}
-            }
-            in.close();  // ƒtƒ@ƒCƒ‹‚ğ•Â‚¶‚é
+	            // å¤‰æ•°lineã«1è¡Œãšã¤èª­ã¿è¾¼ã‚€foræ–‡
+	            for (String line = in.readLine(); line != null; line = in.readLine()) {
+	            	Unifier u= new Unifier(arg[n],line);
+	            	
+			            	if(u.tf == true) 	
+			            	{	
+			            		//System.out.println(line);
+			            		if(u.getV()>0) {
+			            			HashMap<String,String> vars =u.getVars();
+			            			ArrayList var = new ArrayList(vars.values());
+			            			list.get(n).add(var);
+			            			//System.out.println(var.toString());
+			            			//list.get(n).get(countV).add(var);
+			            			countV++;
+			            			if(var.size()>Kmax) Kmax = var.size();
+			            			if(countV>Vmax) Vmax = countV;
+			            		}
+			            			
+			            	}
+	            }
+            in.close();  // ãƒ•ã‚¡ã‚¤ãƒ«ã‚’é–‰ã˜ã‚‹
+	        }
         } catch (IOException e) {
-            e.printStackTrace(); // —áŠO‚ª”­¶‚µ‚½Š‚Ü‚Å‚ÌƒXƒ^ƒbƒNƒgƒŒ[ƒX‚ğ•\¦
+            e.printStackTrace(); // ä¾‹å¤–ãŒç™ºç”Ÿã—ãŸæ‰€ã¾ã§ã®ã‚¹ã‚¿ãƒƒã‚¯ãƒˆãƒ¬ãƒ¼ã‚¹ã‚’è¡¨ç¤º
         }
+		if(argOfV<2) {
+			//System.out.println("wa");
+			int cnt = 0;
+			String ans[][] = new String[argOfV*Vmax][Kmax];
+			//System.out.println(list.get().size());
+			for(int i = 0;i<list.get(list.size()-1).size();i++) {
+				//System.out.println("i");
+				for(int k = 0;k<list.get(list.size()-1).get(i).size();k++) {
+					ans[cnt][k]=list.get(list.size()-1).get(i).get(k);
+					//System.out.println("k");
+				}
+				cnt++;
+			}
+			for(int i = 0;i<ans.length;i++) {
+				System.out.print("(");
+				for(int k = 0;k<ans[0].length;k++) {
+					if(k>0)	System.out.print(", ");
+					System.out.print(ans[i][k]);					
+				}
+				System.out.println(")");
+			}
+		}else {
+			String ans[][] = new String[list.size()*Vmax][Kmax];	//argOfVã«å·®ã—æ›¿ãˆç¢ºèª
+			int cnt = 0;
+			for(int i = 0;i<list.size();i++) {
+				for(int j = 0; j < list.get(i).size();j++) {
+					for(int k = 0; k < list.get(i).get(j).size(); k++) {
+						for(int n = list.size()-i-1;n > 0;n--) {
+							for(int m = list.get(i+n).size()-j-1;m > 0;m--) {
+								if(list.get(i).get(j).get(k).equals(list.get(i+n).get(j+m).get(k))) {
+									for(int h = 0;h<list.get(i+n).get(j+m).size();h++) {
+										ans[cnt][h] = list.get(i+n).get(j+m).get(h);
+									}						
+									cnt++;
+								}
+							}
+						}
+						
+					}
+				}
+			}
+		/*for(int i = 0;i<ans.length;i++) {
+			for(int j = 0;j<ans[i].length;j++) {
+				System.out.println(i+" "+j+ans[i][j]);
+			}
+		}*/
+			
+			for(int i = 0;i<ans.length;i++) {
+				if(ans[i][0]==null) break;
+				System.out.print("(");
+				for(int j = 0;j<ans[i].length;j++) {
+					if(ans[i][j]==null) break;
+					if(j>0)	System.out.print(", ");
+					System.out.print(ans[i][j]);			
+				}
+				System.out.println(")");
+			}
+		}	
 	}
 }
